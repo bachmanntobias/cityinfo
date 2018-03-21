69b99e6efcbf907b1a530e7c9a9550ed0a5673c2
Tobias Bachmann <bachmanntobias96@gmail.com>
2018-03-21 17:24:51 +0100

    213
    Signed-off-by: Tobias Bachmann <bachmanntobias96@gmail.com>

---
+++ CitiesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace CityInfo.API
+{
+    public class CitiesController : Controller
+    {
+        public JsonResult GetCities()
+        {
+            Console.WriteLine(true);
+
+
+            return new JsonResult();    
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace CityInfo.API
 {
@@ -15,7 +16,7 @@ namespace CityInfo.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-           
+            services.AddMvc();
 
         }
 
@@ -27,10 +28,10 @@ namespace CityInfo.API
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            //
+            app.UseMvc();
+
+            
         }
     }
 }
