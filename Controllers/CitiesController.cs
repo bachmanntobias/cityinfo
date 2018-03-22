bcf07116a344694c043d9908a16f9d09ce5ae37d
Tobias Bachmann <bachmanntobias96@gmail.com>
2018-03-22 10:37:57 +0100

    r
    Signed-off-by: Tobias Bachmann <bachmanntobias96@gmail.com>

---
+++ Controllers/CitiesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace CityInfo.API 
+{ 
+    public class CitiesController : Controller
+    {
+        public JsonResult GetCities()
+        {
+            [ HttpGet("api/cities")]
+
+            return new JsonResult(new List<object>()
+            {
+                new {id=1, Name="New York City"},
+                new {id=2, Name="Antwerp"}
+            }); 
+        }
+    }
+}
+/*
+ * GET
+ * POST
+ * PUT
+ * PATCH
+ * DELETE
+ * 
+ */
+
